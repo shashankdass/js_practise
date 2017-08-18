a1497673ce186882663127aec6bc1e475b5f68ee
shashankdass <shashankdass@gmail.com>
2017-08-17 19:35:54 -0700

    JS concepts

---
+++ rules_of_this.js
@@ -0,0 +1,28 @@
+// Rule 1 : if initialized by new, 'this' represent the brand new object inside
+// eg :
+function ConsoleLog (){
+// console.log(this)
+this.value = 10;
+// console.log(this)
+this.dummy = function(){
+  console.log(this);
+  return 2;
+}
+}
+cl = new ConsoleLog();
+// cl.dummy
+
+// Rule 2: if function called using call, bind, apply this refers to the object passed
+// eg
+
+// var obj = {
+//   value:5
+// };
+// ConsoleLog.call(obj);
+// ConsoleLog.apply(obj);
+// bound = ConsoleLog.bind(obj);
+// bound();
+
+// Rule 3: if function called with 'dot', the object calling it becomes this
+
+console.log(cl.dummy());
